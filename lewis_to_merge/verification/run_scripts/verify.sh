8327eacbf519edb70adef758892eb921b52bb133
ChristinaTan0704 <christinatan0704@gmail.com>
2021-02-14 23:32:24 -0800

    save

--- lewis_to_merge/verification/run_scripts/verify.sh
+++
@@ -1,3 +0,0 @@
-"python verify_img_info.py \
-    --pcd_save_folder /Volumes/Data/paper/Rebuttal/m3d_verify/pcd \
-    --verification_progress_save_folder /Volumes/Data/paper/Rebuttal/m3d_verify/progress"
