d2bbe264bf4fd0ff97455f6864c0bee29be8aeeb
ChristinaTan0704 <christinatan0704@gmail.com>
2021-02-22 21:21:09 -0800

    save

--- init_depth_generator/VNL_Monocular_Depth_Prediction/lib/utils/misc.py
+++
@@ -1,8 +0,0 @@
-import socket
-from datetime import datetime
-
-
-def get_run_name():
-    """ A unique name for each run """
-    return datetime.now().strftime(
-        '%b%d-%H-%M-%S') + '_' + socket.gethostname()
