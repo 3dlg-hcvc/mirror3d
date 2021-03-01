844a6568536afaaa0471f41bce811fd5fd06ed3a
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-02-28 18:23:27 -0800

    add scripts

--- script/nyu_train.sh
+++ script/nyu_train.sh
@@ -2,59 +2,101 @@
 python init_depth_generator/bts/pytorch/init_depth_gen_train.py \
 --refined_depth \
 --coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
---coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
 --coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_focal_len 519 \
 --depth_shift 1000 \
 --input_height 480 \
 --input_width 640 \
---batch_size 2 \
---learning_rate 1e-5 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
 --log_directory output
 
 # bts train on sensor depth
 python init_depth_generator/bts/pytorch/init_depth_gen_train.py \
 --coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
---coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
 --coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_focal_len 519 \
 --depth_shift 1000 \
 --input_height 480 \
 --input_width 640 \
---batch_size 2 \
---learning_rate 1e-5 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
 --log_directory output
 
-# bts train on refined depth
+# vnl train on refined depth
 python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_train.py \
 --refined_depth \
 --coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
---coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
 --coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_focal_len 519 \
 --depth_shift 1000 \
 --input_height 480 \
 --input_width 640 \
---batch_size 2 \
---learning_rate 1e-5 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
 --log_directory output
 
 
-# bts train on sensor depth
+# vnl train on sensor depth
 python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_train.py \
 --coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
---coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
 --coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
 --coco_focal_len 519 \
 --depth_shift 1000 \
 --input_height 480 \
 --input_width 640 \
---batch_size 2 \
---learning_rate 1e-5 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--log_directory output
+
+# saic train on refined depth
+python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
+--refined_depth \
+--coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
+--coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
+--coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
+--coco_focal_len 519 \
+--depth_shift 1000 \
+--input_height 480 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--log_directory output
+
+
+# saic train on sensor depth
+python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
+--coco_train dataset/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
+--coco_val dataset/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
+--coco_train_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
+--coco_val_root /local-scratch/jiaqit/exp/Mirror3D/dataset/nyu \
+--coco_focal_len 519 \
+--depth_shift 1000 \
+--input_height 480 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
 --log_directory output
 
 
