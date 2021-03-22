57c8da424092560e989ed7185cb796d42df2c8e5
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-03-22 11:36:48 -0700

    add run script not to release
    Former-commit-id: da01fc9910bef2b27d9f74842f27b099def198c6

--- other_script/copy_rgbd_mask_by_id.py
+++ other_script/copy_rgbd_mask_by_id.py
@@ -14,11 +14,11 @@ import argparse
 
 
 
-def copy_rgbd_mask_by_id(id_list_path, data_main_folder, dst_folder):
+def copy_rgbd_mask_by_id(id_list, data_main_folder, dst_folder):
     is_m3d = False
     if data_main_folder.find("m3d") > 0:
         is_m3d = True
-    id_list = set(read_txt(id_list_path))
+    
 
     raw_folder = os.path.join(data_main_folder, "raw")
     instance_mask_folder = os.path.join(data_main_folder, "instance_mask")
@@ -56,9 +56,27 @@ def copy_rgbd_mask_by_id(id_list_path, data_main_folder, dst_folder):
                                                             os.path.exists(mesh_raw_depth_path), mesh_raw_depth_path))
 
 
-    
+def get_error_sample_rgbd_mask(data_main_folder, dst_folder, current_raw_folder):
+
+    all_raw_folder = os.path.join(data_main_folder, "raw")
+    current_raw_name_list = [i for i in os.listdir(current_raw_folder)]
+    current_raw_name_list.sort()
+
+    all_raw_name_list = [i for i in os.listdir(all_raw_folder)]
+    all_raw_name_list.sort()
+
+    lack_sample_name = list(set(all_raw_name_list) - set(current_raw_name_list))
+    lack_sample_name.sort()
+
+    lack_id_list = [i.split(".")[0] for i in lack_sample_name]
+    lack_id_list = set(lack_id_list)
+
+    copy_rgbd_mask_by_id(lack_id_list, data_main_folder, dst_folder)
 
 
+def count_unique_line_num(txt_path):
+    lines = read_txt(txt_path)
+    print("all num : {} unique num : {}".format(len(lines), len(set(lines))))
 
 
 
@@ -68,15 +86,23 @@ if __name__ == "__main__":
     parser.add_argument(
         '--stage', default="1")
     parser.add_argument(
+        '--txt_path', default="")
+    parser.add_argument(
         '--id_list_path', default="")
     parser.add_argument(
         '--data_main_folder', default="", help="folder that have raw/ instance_mask/ hole_raw_depth .. as sub-folders")
     parser.add_argument(
         '--dst_folder', default="", help="folder to store new raw/ instance_mask/ hole_raw_depth .. as sub-folders")
+    parser.add_argument(
+        '--current_raw_folder', default="", help="raw folder that contains color images")
     args = parser.parse_args()
 
     if args.stage == "1":
-        copy_rgbd_mask_by_id(args.id_list_path, args.data_main_folder, args.dst_folder)
-
+        id_list = set(read_txt(args.id_list_path))
+        copy_rgbd_mask_by_id(id_list, args.data_main_folder, args.dst_folder)
+    elif args.stage == "2":
+        get_error_sample_rgbd_mask(args.data_main_folder, args.dst_folder, args.current_raw_folder)
+    elif args.stage == "3":
+        count_unique_line_num(args.txt_path)
 
 
