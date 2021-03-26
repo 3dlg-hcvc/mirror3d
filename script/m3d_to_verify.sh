85462a93308ae249d165976281aac74e31f37e56
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-03-25 17:19:20 -0700

    save m3d run env
    Former-commit-id: 199557cad69cfb98aa89f9b0e9abfeec0fbd7828

--- script/m3d_to_verify.sh
+++ script/m3d_to_verify.sh
@@ -1,32 +1,26 @@
 #!/bin/zsh
-log_folder=/project/3dlg-hcvc/jiaqit/waste/log/m3d_final_3800_re_anno
+log_folder=/project/3dlg-hcvc/jiaqit/waste/log/m3d_final_2
 mkdir -p $log_folder
 echo $log_folder
 
 parallel -j 20 --eta "python /local-scratch/jiaqit/exp/Mirror3D/annotation/plane_annotation_tool/plane_annotation_tool.py \
 --stage 8 \
---color_img_list /project/3dlg-hcvc/mirrors/www/Mirror3D_final/other_info/m3d_all_color_img_list.txt \
---data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
---anno_output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
---f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_3800_re_anno.log" ::: {0..300}
+--color_img_list /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/raw_img_list.txt \
+--data_main_folder /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise \
+--anno_output_folder /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise \
+--f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_2.log" ::: {0..4500}
 
 # parallel -j 20 --eta "python /local-scratch/jiaqit/exp/Mirror3D/annotation/plane_annotation_tool/plane_annotation_tool.py \
-# --stage 6 \
-# --data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --anno_output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_3800_re_anno.log" ::: {0..300}
+# --stage 3 \
+# --data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_reaano2_part_mask/with_mirror/precise \
+# --anno_output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_reaano2_part_mask/with_mirror/precise \
+# --f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_2.log" ::: {0..4500}
 
 
 # parallel -j 20 --eta "python /local-scratch/jiaqit/exp/Mirror3D/visualization/dataset_visualization.py \
 # --stage all \
-# --data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_3800_re_anno.log" ::: {0..300}
+# --data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_reaano2_part_mask/with_mirror/precise \
+# --output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_reaano2_part_mask/with_mirror/precise \
+# --f 1076 --multi_processing --overwrite --process_index {1} >& ${log_folder}/m3d_final_2.log" ::: {0..4500}
 
 
-
-# parallel -j 20 --eta "python /local-scratch/jiaqit/exp/Mirror3D/visualization/dataset_visualization.py \
-# --stage all \
-# --data_main_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --output_folder /project/3dlg-hcvc/mirrors/www/final_anno/m3d_3800_final/with_mirror/precise \
-# --f 1076 --multi_processing --process_index {1} >& ${log_folder}/m3d_final_3800_re_anno.log" ::: {0..300}
