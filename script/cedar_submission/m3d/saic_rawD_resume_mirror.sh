b2ceadbf44a8cb168f67cd6a4539b7d2b02ce6e9
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-04-03 18:27:41 -0700

    finetune cd5 job script
    Former-commit-id: 21b2cc13f4f2f4e398272a021502df8e40ce6613

---
+++ script/cedar_submission/m3d/saic_rawD_resume_mirror.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=rrg-msavva
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=48000
+#SBATCH --time=0-23:45
+#SBATCH --job-name=saic_rawD_resume_mirror
+#SBATCH --output=/home/jiaqit/projects/rrg-msavva/jiaqit/cr_result/log/%x-%j.out
+source /home/jiaqit/projects/rrg-msavva/jiaqit/setup/anaconda3/bin/activate
+conda activate mirror3d
+export PYTHONPATH="/home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D"
+python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/saic/saic_m3d_meshD_rawD_2021-04-02-16-24-23/checkpoint/snapshot_1_7884.pth \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
--- script/to_release/matterport3d_train.sh
+++ script/to_release/matterport3d_train.sh
@@ -106,6 +106,121 @@ python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
 --log_directory debug
 
 
+# finetune bts train on refined depth
+python init_depth_generator/bts/pytorch/init_depth_gen_train.py \
+--refined_depth \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/bts/bts_m3d_meshD_refinedD_2021-04-02-16-24-17/checkpoint/model-21000 \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+# finetune bts train on sensor depth
+python init_depth_generator/bts/pytorch/init_depth_gen_train.py \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/bts/bts_m3d_meshD_rawD_2021-04-02-16-06-33/checkpoint/model-24000 \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+
+# finetune vnl train on sensor depth
+python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_train.py \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/vnl/vnl_m3d_meshD_rawD_2021-04-02-17-42-25/checkpoint/epoch0_step30000.pth \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+# finetune vnl train on refined depth
+python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_train.py \
+--refined_depth \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/vnl/vnl_m3d_meshD_refinedD_2021-04-02-17-42-25/checkpoint/epoch0_step30000.pth \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+# finetune saic train on sensor depth
+python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/saic/saic_m3d_meshD_rawD_2021-04-02-16-24-23/checkpoint/snapshot_1_7884.pth \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+
+# finetune saic train on refined depth
+python init_depth_generator/saic_depth_completion/init_depth_gen_train.py \
+--refined_depth \
+--resume_checkpoint_path /home/jiaqit/projects/rrg-msavva/jiaqit/exp/Mirror3D/debug/saic/saic_m3d_meshD_refinedD_2021-04-02-16-29-51/tensorboard/snapshot_1_7884.pth \
+--coco_train /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+--coco_val /project/6049211/jiaqit/data/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--coco_train_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_val_root /project/6049211/jiaqit/data/Mirror3D_final/m3d \
+--coco_focal_len 537 \
+--depth_shift 4000 \
+--input_height 512 \
+--input_width 640 \
+--batch_size 8 \
+--checkpoint_save_freq 1500 \
+--num_epochs 100 \
+--learning_rate 1e-4 \
+--mesh_depth \
+--log_directory debug
+
+
+
 
 # planercnn on refD 
 python mirror3dnet/run_mirror3dnet.py \
