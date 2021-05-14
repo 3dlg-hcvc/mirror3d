aa3773313ec14b9004819372c88fbb0545a3cf6b
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-05-13 21:53:01 -0700

    update dataset structure readme
    Former-commit-id: e10b0e93679d0bc150ac923c6d61d95bc04a88b0

---
+++ Mirror3D_dataset.md
@@ -0,0 +1,163 @@
+# Mirror3D 
+
+Mirror3D is a large-scale 3D mirror plane annotation dataset based on three popular RGBD datasets ([Matterpot3D](https://niessner.github.io/Matterport/),[NYUv2](https://cs.nyu.edu/~silberman/datasets/nyu_depth_v2.html), and [ScanNet](http://www.scan-net.org/)) containing 4,852 mirror instance masks and 3D planes.
+
+Please visit our [project website]() for updates and to browse the data.
+
+
+## Mirror3D Data
+
+If you would like to use our data, you must obtain access to the [Matterpot3D](https://niessner.github.io/Matterport/) and [ScanNet](http://www.scan-net.org/)) dataset.  Please send an email to the dataset organizer(s) to confirm your agreement and cc Jiaqi Tan (jiaqit[at]sfu.ca). You will get download links for the data from the dataset that you got approval.
+
+## Mirror3D Data Organization
+
+Mirror3D dataset includes mirror segmentation masks, mirror plane parameters, and refined mirror depth map over 4,852 RGBD images.
+
+
+The downloaded mirror data are stored under the following structure:
+
+
+###NYUv2-small 
+
+For NYUv2-small, the unzipped package's structure is: 
+
+
+```
+nyu
+└── with_mirror # mirror data
+    ├── coarse
+    |   ├── hole_raw_depth # depth maps captured by the depth sensor
+    |   ├── hole_refined_depth # refined sensor depth maps
+    |   ├── img_info # .json file with mirror plane's information; name by mirror color image
+    |   ├── instance_mask # instance level mirror semantic mask
+    |   └── raw # mirror color images
+    └── precise
+        ├── hole_raw_depth
+        ├── hole_refined_depth
+        ├── img_info
+        ├── instance_mask
+        └── raw
+    
+```
+###Matterport3d
+For Matterport3d, the unzipped package's structure is: 
+
+```
+m3d
+├── only_mask # mirror data that only contains mirror masks' information; depth images for these samples are too noisy to annotate. 
+│   ├── coarse
+│   |   ├── instance_mask # instance level mirror semantic mask
+│   |   └── raw # mirror color images
+│   └── precise
+│       ├── instance_mask
+│       └── raw
+└── with_mirror
+    ├── coarse
+    |   ├── hole_raw_depth # depth maps captured by the depth sensor
+    |   ├── hole_refined_depth # refined sensor depth maps
+    |   ├── mesh_raw_depth # mesh depth maps generated from the 3D mesh reconstruction
+    |   ├── mesh_refined_depth # refined mirror mesh depth maps
+    |   ├── img_info # .json file with mirror plane's information; name by mirror color image
+    |   ├── instance_mask # instance level mirror semantic mask
+    |   └── raw # mirror color images
+    └── precise
+        ├── hole_raw_depth
+        ├── hole_refined_depth
+        ├── mesh_raw_depth
+        ├── mesh_refined_depth
+        ├── img_info
+        ├── instance_mask
+        └── raw
+```
+###Scannet
+
+For Scannet, the unzipped package's structure is: 
+
+```
+scannet
+├── only_mask # mirror data that only contains mirror masks' information; depth images for these samples are too noisy to annotate. 
+│   ├── coarse
+│   |   ├── instance_mask
+│   |   └── raw
+│   └── precise
+│       ├── instance_mask
+│       └── raw
+└── with_mirror
+    ├── coarse
+    |   ├── hole_raw_depth # depth maps captured by the depth sensor
+    |   ├── hole_refined_depth # refined sensor depth maps
+    |   ├── img_info # .json file with mirror plane's information; name by mirror color image
+    |   ├── instance_mask # instance level mirror semantic mask
+    |   └── raw # mirror color images
+    └── precise
+        ├── hole_raw_depth
+        ├── hole_refined_depth
+        ├── img_info
+        ├── instance_mask
+        └── raw
+```
+
+#### JSON files for Mirror Plane Information
+
+Here, one mirror sample's plane parameters information is saved in a single JSON file. The information is saved in a format:
+
+```json
+{
+    "0_0_128":{ # 0_0_128 is the instance id (R_G_B of the semantic mask)
+    	# mirror plane parameter in 3D; 
+    	# here y axis points upward, -z axis points to the front
+        "plane_parameter":[ 
+            0.00025589483339543795,
+            6.575998812963738e-05,
+            -0.00026007778514475276,
+            0.9999999312764996
+        ],
+        # mirror plane's normal 
+        "mirror_normal":[
+            0.00025589483339543795,
+            6.575998812963738e-05,
+            -0.00026007778514475276
+        ]
+    }
+}
+
+```
+
+
+## Training Data Structure
+
+To train or test the models with the .json input file we provided, you need to download the original data from the relevant dataset and store these original data under the following structure:
+
+###NYUv2-small
+
+```
+nyu
+├── original_dataset # please create a folder named "original_dataset" and put the original dataset's data under this folder if you want to train our models.
+│   ├── color
+│   └── depth
+└── with_mirror # downloaded mirror data
+        
+```
+
+###Matterport3d
+```
+m3d
+├── original_dataset # please create a folder named "original_dataset" and put the original dataset's data under this folder if you want to train our models.
+│   ├── matterport_render_depth
+│   ├── undistorted_color_images
+│   └── undistorted_depth_images
+├── only_mask # downloaded mirror data
+└── with_mirror # downloaded mirror data
+        
+```
+
+###Scannet
+
+```
+scannet
+├── original_dataset # please create a folder named "original_dataset" and put the original dataset's data under this folder if you want to train our models.
+│   └── scannet_frames_25k
+├── only_mask # downloaded mirror data
+└── with_mirror # downloaded mirror data
+        
+```
--- script/to_release/matterport3d_infer.sh
+++ script/to_release/matterport3d_infer.sh
@@ -503,70 +503,70 @@ python mirror3dnet/run_mirror3dnet.py \
 
 
 
-echo "rawD + Mirror3dNet 0"
-python mirror3dnet/run_mirror3dnet.py \
---eval \
---resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_0.pth \
---config mirror3dnet/config/mirror3dnet_normal_config.yml \
---refined_depth \
---coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
---coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
---coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
---coco_focal_len 537 \
---depth_shift 4000 \
---input_height 512 \
---input_width 640 \
---batch_size 1 \
---checkpoint_save_freq 1500 \
---num_epochs 100 \
---learning_rate 1e-4 \
---log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
---ref_mode rawD_border
+# echo "rawD + Mirror3dNet 0"
+# python mirror3dnet/run_mirror3dnet.py \
+# --eval \
+# --resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_0.pth \
+# --config mirror3dnet/config/mirror3dnet_normal_config.yml \
+# --refined_depth \
+# --coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+# --coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+# --coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
+# --coco_focal_len 537 \
+# --depth_shift 4000 \
+# --input_height 512 \
+# --input_width 640 \
+# --batch_size 1 \
+# --checkpoint_save_freq 1500 \
+# --num_epochs 100 \
+# --learning_rate 1e-4 \
+# --log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
+# --ref_mode rawD_border
 
 
-echo "rawD + Mirror3dNet 1"
-python mirror3dnet/run_mirror3dnet.py \
---eval \
---resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_1.pth \
---config mirror3dnet/config/mirror3dnet_normal_config.yml \
---refined_depth \
---coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
---coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
---coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
---coco_focal_len 537 \
---depth_shift 4000 \
---input_height 512 \
---input_width 640 \
---batch_size 1 \
---checkpoint_save_freq 1500 \
---num_epochs 100 \
---learning_rate 1e-4 \
---log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
---ref_mode rawD_border
+# echo "rawD + Mirror3dNet 1"
+# python mirror3dnet/run_mirror3dnet.py \
+# --eval \
+# --resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_1.pth \
+# --config mirror3dnet/config/mirror3dnet_normal_config.yml \
+# --refined_depth \
+# --coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+# --coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+# --coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
+# --coco_focal_len 537 \
+# --depth_shift 4000 \
+# --input_height 512 \
+# --input_width 640 \
+# --batch_size 1 \
+# --checkpoint_save_freq 1500 \
+# --num_epochs 100 \
+# --learning_rate 1e-4 \
+# --log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
+# --ref_mode rawD_border
 
 
-echo "rawD + Mirror3dNet 2"
-python mirror3dnet/run_mirror3dnet.py \
---eval \
---resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_2.pth \
---config mirror3dnet/config/mirror3dnet_normal_config.yml \
---refined_depth \
---coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
---coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
---coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
---anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
---coco_focal_len 537 \
---depth_shift 4000 \
---input_height 512 \
---input_width 640 \
---batch_size 1 \
---checkpoint_save_freq 1500 \
---num_epochs 100 \
---learning_rate 1e-4 \
---log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
---ref_mode rawD_border
+# echo "rawD + Mirror3dNet 2"
+# python mirror3dnet/run_mirror3dnet.py \
+# --eval \
+# --resume_checkpoint_path /project/3dlg-hcvc/mirrors/www/final_result/checkpoint/m3d_final/m3n_normal_10_2.pth \
+# --config mirror3dnet/config/mirror3dnet_normal_config.yml \
+# --refined_depth \
+# --coco_train /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/train_10_normal_mirror.json \
+# --coco_val /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+# --coco_train_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --coco_val_root /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
+# --anchor_normal_npy /project/3dlg-hcvc/mirrors/www/Mirror3D_final/mirror_normal/m3d/m3d_kmeans_normal_10.npy \
+# --coco_focal_len 537 \
+# --depth_shift 4000 \
+# --input_height 512 \
+# --input_width 640 \
+# --batch_size 1 \
+# --checkpoint_save_freq 1500 \
+# --num_epochs 100 \
+# --learning_rate 1e-4 \
+# --log_directory /project/3dlg-hcvc/mirrors/www/final_result/m3d_final \
+# --ref_mode rawD_border
