bd55a0059ac30bfb6448dccf1aef0e57779e7d21
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-05-16 22:02:59 -0700

    add infer
    Former-commit-id: c0f4bacfc393506c5919a7e506cfc315e77057a6

--- script/cedar_submission/nyu_infer/nyu_dg_infer.sh
+++ script/cedar_submission/nyu_infer/nyu_dg_infer.sh
@@ -167,3 +167,28 @@ python init_depth_generator/saic_depth_completion/init_depth_gen_infer.py \
 --output_save_folder /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/output/final_result/nyu_final
 
 
+# bts test on official checkpoint
+python init_depth_generator/bts/pytorch/init_depth_gen_infer.py \
+--resume_checkpoint_path /project/6049211/jiaqit/exp/Mirror3D/checkpoint/nyu_final/bts_official_nyu_raw \
+--coco_train /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
+--coco_val /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
+--coco_train_root /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu \
+--coco_val_root /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu \
+--coco_focal_len 519 \
+--depth_shift 1000 \
+--input_height 480 \
+--input_width 640 \
+--output_save_folder /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/output/final_result/nyu_final
+
+# vnl test on official checkpoint
+python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_infer.py \
+--resume_checkpoint_path /project/6049211/jiaqit/exp/Mirror3D/checkpoint/nyu_final/vnl_official_nyu_raw.pth \
+--coco_train /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu/with_mirror/precise/network_input_json/train_10_normal_all.json \
+--coco_val /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
+--coco_train_root /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu \
+--coco_val_root /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/nyu \
+--coco_focal_len 519 \
+--depth_shift 1000 \
+--input_height 480 \
+--input_width 640 \
+--output_save_folder /home/jiaqit/projects/rrg-msavva/jiaqit/data/Mirror3D_final/output/final_result/nyu_final
