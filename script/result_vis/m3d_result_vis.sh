1d0015cc86dbcce663ebc20b0bfe904255525ba4
jiaqit <jiaqit@cs-vml-17.cmpt.sfu.ca>
2021-05-21 23:33:52 -0700

    infer update
    Former-commit-id: c600db6d5e45e46550e3ba2f4aef3394b980c9c0
    Former-commit-id: fc693f5ca9a2b667404fd7e63f916fe6c67e4c1b

--- script/result_vis/m3d_result_vis.sh
+++ script/result_vis/m3d_result_vis.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
-log_folder=/project/3dlg-hcvc/jiaqit/waste/log/m3d_vis_0518
+log_folder=/project/3dlg-hcvc/jiaqit/waste/log/m3d_vis_0521
 mkdir -p $log_folder
 echo $log_folder
 
 parallel -j 20 --eta "python visualization/result_visualization.py \
 --stage 1 \
 --multi_processing --process_index {1} \
---method_predFolder_txt /project/3dlg-hcvc/mirrors/www/notes/m3d_vis_0518.txt \
+--method_predFolder_txt /project/3dlg-hcvc/mirrors/www/notes/m3d_vis_0521.txt \
 --dataset_main_folder /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d \
 --f 537 \
 --pred_w 640 \
 --pred_h 512 \
---test_json /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/test_10_normal_mirror.json \
---output_folder /project/3dlg-hcvc/mirrors/www/final_result_vis/m3d_result_vis >& ${log_folder}/m3d_vis_0518.log" ::: {0..660}
+--test_json /project/3dlg-hcvc/mirrors/www/Mirror3D_final/m3d/with_mirror/precise/network_input_json/val_10_normal_mirror.json \
+--output_folder /project/3dlg-hcvc/mirrors/www/final_result_vis/m3d_result_vis_val >& ${log_folder}/m3d_vis_0521.log" ::: {0..660}
---
+++ utils/dataset_pack.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+import argparse
+import os
+import matplotlib.pyplot as plt
+from skimage import io
+import sys
+from utils.algorithm import *
+from utils.general_utlis import *
+from utils.plane_pcd_utils import *
+import json
+import shutil
+from annotation.plane_annotation.plane_annotation_tool import *
+
+ 
+
+ def reformat_json(input_folder, output_folder):
+     pass
+
+
+
+
+
+if __name__ == "__main__":
+    input_folder = "/project/3dlg-hcvc/mirrors/www/Mirror3D_final/nyu/with_mirror/precise/img_info"
+    output_folder = ""
+    reformat_json(input_folder, output_folder)
+    
