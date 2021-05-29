401f9cc92aa02490cc9b4309371bce2eb472333c
jiaqit <jiaqit@cs-3dlg-10.cmpt.sfu.ca>
2021-05-29 08:31:18 -0700

    update train script
    Former-commit-id: edc9d7b4d90d7ff068a4768b37e8d81277ccad37 [formerly 637e6ae06aa91427efb05fa502963390f753c98e]
    Former-commit-id: 0cddcac4e6dcbdf7c4b4c11d2c10b527c32a7fe4
    Former-commit-id: 9e3c3b28c35508c5405d74ee773862e67c44da78

--- script/nyu_infer.sh
+++ script/nyu_infer.sh
@@ -1,21 +1,7 @@
 # bts infer on refined sensor depth
 python init_depth_generator/bts/pytorch/init_depth_gen_infer.py \
---resume_checkpoint_path ./checkpoint/
+--resume_checkpoint_path ./checkpoint/nyu/bts_refD \
 --refined_depth \
---coco_val ./network_input_json/test_10_normal_mirror.json \
---coco_val_root ./dataset/nyu \
---coco_focal_len 519 \
---depth_shift 1000 \
---input_height 480 \
---input_width 640 \
---batch_size 8 \
---checkpoint_save_freq 1500 \
---num_epochs 100000 \
---learning_rate 1e-4 \
---log_directory output/nyu
-
-# bts infer on raw sensor depth
-python init_depth_generator/bts/pytorch/init_depth_gen_infer.py \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
 --coco_focal_len 519 \
@@ -30,6 +16,7 @@ python init_depth_generator/bts/pytorch/init_depth_gen_infer.py \
 
 # vnl infer on refined sensor depth
 python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_infer.py \
+--resume_checkpoint_path ./checkpoint/nyu/vnl_refD.pth \
 --refined_depth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
@@ -44,22 +31,9 @@ python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_infer.
 --log_directory output/nyu
 
 
-# vnl infer on raw sensor depth
-python init_depth_generator/VNL_Monocular_Depth_Prediction/init_depth_gen_infer.py \
---coco_val ./network_input_json/test_10_normal_mirror.json \
---coco_val_root ./dataset/nyu \
---coco_focal_len 519 \
---depth_shift 1000 \
---input_height 480 \
---input_width 640 \
---batch_size 4 \
---checkpoint_save_freq 1500 \
---num_epochs 100000 \
---learning_rate 1e-4 \
---log_directory output/nyu
-
 # saic infer on refined sensor depth
 python init_depth_generator/saic_depth_completion/init_depth_gen_infer.py \
+--resume_checkpoint_path ./checkpoint/nyu/saic_refD.pth \
 --refined_depth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
@@ -76,6 +50,7 @@ python init_depth_generator/saic_depth_completion/init_depth_gen_infer.py \
 
 # saic infer on raw sensor depth
 python init_depth_generator/saic_depth_completion/init_depth_gen_infer.py \
+--resume_checkpoint_path ./checkpoint/nyu/saic_rawD.pth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
 --coco_focal_len 519 \
@@ -91,7 +66,7 @@ python init_depth_generator/saic_depth_completion/init_depth_gen_infer.py \
 
 # planercnn on refined sensor depth
 python mirror3dnet/run_mirror3dnet.py \
---resume_checkpoint_path ./checkpoint/R-50.pkl \
+--resume_checkpoint_path ./checkpoint/mp3d/planercnn_refD.pth \
 --config mirror3dnet/config/planercnn_config.yml \
 --refined_depth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
@@ -110,7 +85,7 @@ python mirror3dnet/run_mirror3dnet.py \
 
 # planercnn on raw sensor depth
 python mirror3dnet/run_mirror3dnet.py \
---resume_checkpoint_path ./checkpoint/R-50.pkl \
+--resume_checkpoint_path ./checkpoint/mp3d/planercnn_rawD.pth \
 --config mirror3dnet/config/planercnn_config.yml \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
@@ -131,7 +106,7 @@ python mirror3dnet/run_mirror3dnet.py \
 python mirror3dnet/run_mirror3dnet.py \
 --config mirror3dnet/config/mirror3dnet_config.yml \
 --refined_depth \
---resume_checkpoint_path ./checkpoint/R-50.pkl \
+--resume_checkpoint_path ./checkpoint/mp3d/mirror3dnet_refD.pth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
 --coco_focal_len 519 \
@@ -149,7 +124,7 @@ python mirror3dnet/run_mirror3dnet.py \
 # mirror3dnet on raw sensor depth 
 python mirror3dnet/run_mirror3dnet.py \
 --config mirror3dnet/config/mirror3dnet_config.yml \
---resume_checkpoint_path ./checkpoint/R-50.pkl \
+--resume_checkpoint_path ./checkpoint/mp3d/mirror3dnet_rawD.pth \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
 --coco_focal_len 519 \
@@ -165,6 +140,7 @@ python mirror3dnet/run_mirror3dnet.py \
 
 # mirror3dnet only normal 10 anchor normal
 python mirror3dnet/run_mirror3dnet.py \
+--resume_checkpoint_path ./checkpoint/mp3d/mirror3dnet_normal_10.pth \
 --config mirror3dnet/config/mirror3dnet_normal_config.yml \
 --coco_val ./network_input_json/test_10_normal_mirror.json \
 --coco_val_root ./dataset/nyu \
@@ -177,6 +153,5 @@ python mirror3dnet/run_mirror3dnet.py \
 --checkpoint_save_freq 1500 \
 --num_epochs 100000 \
 --learning_rate 1e-4 \
---resume_checkpoint_path ./checkpoint/R-50.pkl \
 --anchor_normal_npy ./dataset/mirror_normal/mp3d_kmeans_normal_10.npy \
 --log_directory output/nyu
