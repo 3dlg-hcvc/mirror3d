96b51e21351d144f332a5586cc1c361816e80271
jiaqit <jiaqit@cs-3dlg-10.cmpt.sfu.ca>
2021-06-02 16:42:29 -0700

    fix bug for generate refined sensorD
    Former-commit-id: 8c50bc7b9d45ffc669aedc7c10003b4732575fae [formerly 43277b8b93217ccf52592361aac6288a0b035f7e]
    Former-commit-id: b23fa25a37fbced96344b8e11367776d505eb2a4
    Former-commit-id: abb6b97d3f39147c2089aeb111954dd10cf6e4d5

--- dataset/gen_refinedD_from_delta.py
+++ dataset/gen_refinedD_from_delta.py
@@ -23,19 +23,38 @@ def gen_refinedD_from_deltaD(unzipped_folder_path, mask_version):
             cv2.imwrite(one_ref_d_path, refined_d.astype(np.uint16))
         print("refined depth saved to:", ref_d_folder)
 
-    raw_d_folder = os.path.join(unzipped_folder_path, "raw_sensorD")
-    delta_img_folder = os.path.join(unzipped_folder_path, "delta_depth_{}".format(mask_version))
-    ref_d_folder = os.path.join(unzipped_folder_path, "refined_sensorD_{}".format(mask_version))
-    delta_d_list = [i.strip() for i in os.popen("find {} -type f".format(delta_img_folder)).readlines()]
-    os.makedirs(ref_d_folder, exist_ok=True)
-    for one_delta_path in tqdm(delta_d_list):
-        one_raw_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version), "raw_sensorD")
-        refined_d = cv2.imread(one_raw_d_path, cv2.IMREAD_ANYDEPTH) + cv2.imread(one_delta_path, cv2.IMREAD_ANYDEPTH)
-        one_ref_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version),
-                                                "refined_sensorD_{}".format(mask_version))
-        os.makedirs(os.path.split(one_ref_d_path)[0], exist_ok=True)
-        cv2.imwrite(one_ref_d_path, refined_d.astype(np.uint16))
-    print("refined depth saved to:", ref_d_folder)
+        raw_d_folder = os.path.join(unzipped_folder_path, "raw_sensorD")
+        delta_img_folder = os.path.join(unzipped_folder_path, "delta_depth_{}".format(mask_version))
+        ref_d_folder = os.path.join(unzipped_folder_path, "refined_sensorD_{}".format(mask_version))
+        delta_d_list = [i.strip() for i in os.popen("find {} -type f".format(delta_img_folder)).readlines()]
+        os.makedirs(ref_d_folder, exist_ok=True)
+        for one_delta_path in tqdm(delta_d_list):
+            one_mask_path = rreplace(one_delta_path.replace("delta_depth_{}".format(mask_version), "mirror_instance_mask_{}".format(mask_version)), "d", "i")
+            mirror_mask = cv2.imread(one_mask_path, cv2.IMREAD_ANYDEPTH)
+            one_raw_sensor_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version), "raw_sensorD")
+            one_ref_mesh_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version),
+                                        "refined_meshD_{}".format(mask_version))
+            refined_d = cv2.imread(one_raw_sensor_d_path, cv2.IMREAD_ANYDEPTH)*((mirror_mask==0).astype(np.uint8)) + \
+                        cv2.imread(one_ref_mesh_d_path, cv2.IMREAD_ANYDEPTH)*((mirror_mask>0).astype(np.uint8))
+            one_ref_sensor_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version),
+                                                    "refined_sensorD_{}".format(mask_version))
+            os.makedirs(os.path.split(one_ref_sensor_d_path)[0], exist_ok=True)
+            cv2.imwrite(one_ref_sensor_d_path, refined_d.astype(np.uint16))
+        print("refined depth saved to:", ref_d_folder)
+    else:
+        raw_d_folder = os.path.join(unzipped_folder_path, "raw_sensorD")
+        delta_img_folder = os.path.join(unzipped_folder_path, "delta_depth_{}".format(mask_version))
+        ref_d_folder = os.path.join(unzipped_folder_path, "refined_sensorD_{}".format(mask_version))
+        delta_d_list = [i.strip() for i in os.popen("find {} -type f".format(delta_img_folder)).readlines()]
+        os.makedirs(ref_d_folder, exist_ok=True)
+        for one_delta_path in tqdm(delta_d_list):
+            one_raw_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version), "raw_sensorD")
+            refined_d = cv2.imread(one_raw_d_path, cv2.IMREAD_ANYDEPTH) + cv2.imread(one_delta_path, cv2.IMREAD_ANYDEPTH)
+            one_ref_d_path = one_delta_path.replace("delta_depth_{}".format(mask_version),
+                                                    "refined_sensorD_{}".format(mask_version))
+            os.makedirs(os.path.split(one_ref_d_path)[0], exist_ok=True)
+            cv2.imwrite(one_ref_d_path, refined_d.astype(np.uint16))
+        print("refined depth saved to:", ref_d_folder)
 
 
 if __name__ == "__main__":
