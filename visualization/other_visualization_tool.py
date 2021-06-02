1bbb31d2f743fd2c94c7339d5657c36d6c9d1565
jiaqit <jiaqit@cs-3dlg-10.cmpt.sfu.ca>
2021-06-02 16:42:29 -0700

    fix bug for generate refined sensorD
    Former-commit-id: a0f52de798d923a9fe79a9a1c7653ad7b3173e8b [formerly 332bb4db6992b262c497c2883eb83c1c5b9ac187]
    Former-commit-id: 422576eda71e74dfd126bc3a845c58bf0a326b0d
    Former-commit-id: 533e46c7ea2c47b3b43e84bae20d281a503805e7

--- visualization/other_visualization_tool.py
+++ visualization/other_visualization_tool.py
@@ -30,14 +30,23 @@ def visualize_pcd():
 def visualize_pcd_from_rgbd(depth_img_path, color_img_path, f, save_folder=""):
     pcd = get_pcd_from_rgbd_depthPath(f, depth_img_path, color_img_path)
 
+    
+    print("visualizing {} {}".format(color_img_path, depth_img_path))
+    xyz = np.array(pcd.points)
+    X = xyz[:, 0]
+    Y = xyz[:, 1]
+    Z = xyz[:, 2]
+
+    print("X max {:.2f} X min {:.2f} X gap {:.2f}".format(X.max(), X.min(), X.max() - X.min()))
+    print("Y max {:.2f} Y min {:.2f} Y gap {:.2f}".format(Y.max(), Y.min(), Y.max() - Y.min()))
+    print("Z max {:.2f} Z min {:.2f} Z gap {:.2f}".format(Z.max(), Z.min(), Z.max() - Z.min()))
+    o3d.visualization.draw_geometries([pcd])
+
     if os.path.exists(save_folder):
         pcd_save_path = os.path.join(save_folder, "{}.ply".format(color_img_path.split("/")[-1].split(".")[0]))
         o3d.io.write_point_cloud(pcd_save_path, pcd)
         print("pcd saved to {}".format(pcd_save_path))
-    else:
-        print("visualizing {}".format(color_img_path))
-        o3d.visualization.draw_geometries([pcd])
-
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get Setting :D')
